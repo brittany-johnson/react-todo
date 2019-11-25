460197c32e6e5382e278a0ec663edc0a7a18701c
brittany-johnson <brittanyj.email@gmail.com>
2019-11-24 19:58:27 -0600

    Updates

---
+++ precache-manifest.812b5a4a031a9bec1c5b9c4eb983664c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8b8c73c4420721b0a3819b0e6ef360",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "86d8881d9909c28e6100",
+    "url": "/react-todo/static/css/main.bb9a17fa.chunk.css"
+  },
+  {
+    "revision": "89340a2db79e1eace225",
+    "url": "/react-todo/static/js/2.dda22e83.chunk.js"
+  },
+  {
+    "revision": "86d8881d9909c28e6100",
+    "url": "/react-todo/static/js/main.deb5ce5c.chunk.js"
+  },
+  {
+    "revision": "460e1f2afce5d90d8bdc",
+    "url": "/react-todo/static/js/runtime-main.8c3598db.js"
+  }
+]);
